17fb6a72f85d67e08bfae6be7ab83965592c84fc
jd-hyun <wjdehdgus3540@naver.com>
2023-03-21 19:19:29 +0900

    [Fix] 멤버 조회 오류 수정 - #16

--- book-service/src/main/java/com/example/bookservice/controller/MemberController.java
+++ book-service/src/main/java/com/example/bookservice/controller/MemberController.java
@@ -37,20 +37,26 @@ public class MemberController {
     }
 
     // 회원 조회
-    @GetMapping("/{memberId}")
-    public MemberResponse findById(@PathVariable Long memberId) {
-        log.info("[findById] 회원 조회를 수행합니다. id : {}", memberId);
-        Member member = memberService.findById(memberId);
-        log.info("[findById] 회원 조회를 완료했습니다. id : {}", member.getId());
+    @GetMapping
+    public MemberResponse findById() {
+        log.info("[findById] 회원 조회를 수행합니다.");
+        Member member = getMember();
+        log.info("[findById] 회원 조회를 완료했습니다. id : {}", getMember().getId());
         return new MemberResponse(member);
     }
 
     // 회원 삭제
-    @DeleteMapping("/{memberId}")
-    public ResponseResult delete(@PathVariable Long memberId) {
-        log.info("[delete] 회원 탈퇴를 수행합니다. id : {}" , memberId);
-        memberService.delete(memberId);
-
+    @DeleteMapping
+    public ResponseResult delete() {
+        log.info("[delete] 회원 탈퇴를 수행합니다.");
+        Member member = getMember();
+        memberService.delete(member.getId());
+        log.info("[delete] 회원 탈퇴를 완료했습니다. id : {}", member.getId());
         return new ResponseResult(true, "회원 탈퇴 완료");
+    }
+
+    private static Member getMember() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return  (Member)authentication.getPrincipal();
     }
 }
